64383f245f3b78654a59a365be66483f418b3946
ThisIsTAEWON <125544913+ThisIsTAEWON@users.noreply.github.com>
2024-12-02 22:26:37 +0900

    앱 상태 변수로 데이터 저장 방식 변경

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import os
 import pickle
 
 FILENAME = "plant_data.p"
-os.chmod(FILENAME, 0o777)
 app = FastAPI()
 
 class PlantStatusData(BaseModel):
@@ -13,33 +11,40 @@ class PlantStatusData(BaseModel):
     humidity: float = None
     illuminance: float = None
 
+
+@app.on_event("startup")
+async def startup():
+    app.state.data = PlantStatusData()
 @app.get("/")
 async def root():
-    with open(FILENAME, 'rb') as f:
-        return {"soil_moisture": pickle.load(f),
-                "temperature": pickle.load(f),
-                "humidity": pickle.load(f),
-                "illuminance": pickle.load(f),
-                }
+    # with open(FILENAME, 'rb') as f:
+    #     return {"soil_moisture": pickle.load(f),
+    #             "temperature": pickle.load(f),
+    #             "humidity": pickle.load(f),
+    #             "illuminance": pickle.load(f),
+    #             }
+    return app.state.data
 
 
 @app.post("/update")
 async def update(psd: PlantStatusData):
-    with open(FILENAME, 'wb') as f:
-        pickle.dump(psd.soil_moisture, f)
-        pickle.dump(psd.temperature, f)
-        pickle.dump(psd.humidity, f)
-        pickle.dump(psd.illuminance, f)
+    app.state.data = psd
+    # with open(FILENAME, 'wb') as f:
+    #     pickle.dump(psd.soil_moisture, f)
+    #     pickle.dump(psd.temperature, f)
+    #     pickle.dump(psd.humidity, f)
+    #     pickle.dump(psd.illuminance, f)
 
     return {"success": True, "message": "Data updated"}
 
 @app.get("/update")
 async def update():
-    with open(FILENAME, 'rb') as f:
-        data =  {"soil_moisture": pickle.load(f),
-                "temperature": pickle.load(f),
-                "humidity": pickle.load(f),
-                "illuminance": pickle.load(f),
-                }
+    # with open(FILENAME, 'rb') as f:
+    #     data =  {"soil_moisture": pickle.load(f),
+    #             "temperature": pickle.load(f),
+    #             "humidity": pickle.load(f),
+    #             "illuminance": pickle.load(f),
+    #             }
+    data = app.state.data
 
     return {"success": True, "data": data}
