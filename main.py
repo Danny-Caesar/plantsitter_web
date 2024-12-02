6d2b8014dff5926fd5792bc7e5f0f71e402cd4ae
ThisIsTAEWON <125544913+ThisIsTAEWON@users.noreply.github.com>
2024-12-02 22:42:37 +0900

    레거시 및 불필요한 종속성 제거

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import pickle
 
-FILENAME = "plant_data.p"
 app = FastAPI()
 
 class PlantStatusData(BaseModel):
@@ -17,34 +15,17 @@ async def startup():
     app.state.data = PlantStatusData()
 @app.get("/")
 async def root():
-    # with open(FILENAME, 'rb') as f:
-    #     return {"soil_moisture": pickle.load(f),
-    #             "temperature": pickle.load(f),
-    #             "humidity": pickle.load(f),
-    #             "illuminance": pickle.load(f),
-    #             }
     return app.state.data
 
 
 @app.post("/update")
 async def update(psd: PlantStatusData):
     app.state.data = psd
-    # with open(FILENAME, 'wb') as f:
-    #     pickle.dump(psd.soil_moisture, f)
-    #     pickle.dump(psd.temperature, f)
-    #     pickle.dump(psd.humidity, f)
-    #     pickle.dump(psd.illuminance, f)
 
     return {"success": True, "message": "Data updated"}
 
 @app.get("/update")
 async def update():
-    # with open(FILENAME, 'rb') as f:
-    #     data =  {"soil_moisture": pickle.load(f),
-    #             "temperature": pickle.load(f),
-    #             "humidity": pickle.load(f),
-    #             "illuminance": pickle.load(f),
-    #             }
     data = app.state.data
 
     return {"success": True, "data": data}
